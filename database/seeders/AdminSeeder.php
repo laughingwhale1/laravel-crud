ced7218f0d6edc204b575d6aa6e963ca4b7f0913
rodion <luckypalm95@gmail.com>
2024-08-21 17:54:29 +0300

    add auth with basic styles, remove register, add admin role, add admin middleware

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,6 +14,7 @@ class AdminSeeder extends Seeder
             'name' => 'Admin Admin',
             'email' => 'admin@admin.com',
             'password' => Hash::make('password'),
+            'is_admin' => true,
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes([
+    // disabling the register functionality
+    'register' => false,
+]);
+
+Route::middleware(['auth', 'is_admin'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
+
