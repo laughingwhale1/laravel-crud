44977891c9bac1b9713d49f4bcdc5ec7aaf6c577
rodion <luckypalm95@gmail.com>
2024-08-21 18:12:42 +0300

    rework user migration

--- database/migrations/2024_08_21_145550_create_employees_table.php
+++ database/migrations/2024_08_21_145550_create_employees_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->foreignId('company_id')->constrained('companies');
+            $table->foreignId('company_id')->nullable()->constrained('companies')->onDelete('set null');
             $table->string('email')->nullable();
             $table->string('phone')->nullable();
             $table->timestamps();
